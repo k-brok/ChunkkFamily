78394ccc9cce6e2f306379fdee7b7f081ecb14cd
kbrok <kasper.brok@cardsplmsolutions.com>
2023-11-17 13:42:05 +0100

    WS test

--- index.php
+++ index.php
@@ -4,27 +4,39 @@
 			  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
 			  crossorigin="anonymous"></script>
     <div id="root"></div>
-    <script>  
+<script>  
+	function showMessage(messageHTML) {
+		$('#chat-box').append(messageHTML);
+	}
 
 	$(document).ready(function(){
-		var websocket = new WebSocket("ws://127.0.0.1:1234/websockets.php"); 
+		var websocket = new WebSocket("ws://localhost:8090/demo/php-socket.php"); 
 		websocket.onopen = function(event) { 
-			$("#root").append("<div class='chat-connection-ack'>Connection is established!</div>");		
+			showMessage("<div class='chat-connection-ack'>Connection is established!</div>");		
 		}
 		websocket.onmessage = function(event) {
 			var Data = JSON.parse(event.data);
-			$("#root").append("<div class='"+Data.message_type+"'>"+Data.message+"</div>");
+			showMessage("<div class='"+Data.message_type+"'>"+Data.message+"</div>");
+			$('#chat-message').val('');
 		};
 		
 		websocket.onerror = function(event){
-			$("#root").append("<div class='error'>Problem due to some Error</div>");
-			$("#root").append(event.data);
+			showMessage("<div class='error'>Problem due to some Error</div>");
 		};
 		websocket.onclose = function(event){
-			$("#root").append("<div class='chat-connection-ack'>Connection Closed</div>");
+			showMessage("<div class='chat-connection-ack'>Connection Closed</div>");
 		}; 
+		
+		$('#frmChat').on("submit",function(event){
+			event.preventDefault();
+			$('#chat-user').attr("type","hidden");		
+			var messageJSON = {
+				chat_user: $('#chat-user').val(),
+				chat_message: $('#chat-message').val()
+			};
+			websocket.send(JSON.stringify(messageJSON));
+		});
 	});
 </script>
-
 </body>
 </html>
